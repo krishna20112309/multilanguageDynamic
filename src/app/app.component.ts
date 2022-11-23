b479a796bf2226e6d7125d32f4ee7da5480cdc34
Krishna Kumar <56166609+krishna20112309@users.noreply.github.com>
2022-11-23 15:48:32 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { marker as TRANSLATE_ME } from "@biesbjerg/ngx-translate-extract-marker";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    const val = TRANSLATE_ME('home.title');
+    console.log(' Title from marker ==> ', TRANSLATE_ME('home.title'));
+  }
+
+  supportLanguages = ['en', 'fr', 'ta', 'hi','pn', 'ru'];
+
+  constructor(private translateService: TranslateService){
+    this.translateService.addLangs(this.supportLanguages);
+    this.translateService.setDefaultLang('en');
+
+    const browserlang = this.translateService.getBrowserLang();
+
+    console.log('Browser Language => ', browserlang);
+
+    if (this.supportLanguages.includes(browserlang)) {
+      this.translateService.use(browserlang);
+    }
+  }
+
+  useLang(lang: string) {
+    console.log('selected language ==> ', lang);
+    this.translateService.use(lang);
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TranslateModule, TranslateLoader, TranslateService } from "@ngx-translate/core";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (http: HttpClient) => { return new TranslateHttpLoader(http, './assets/i18n/', '.json'); },
+        deps: [HttpClient]
+      }
+    })
+  ],
+  providers: [TranslateService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
